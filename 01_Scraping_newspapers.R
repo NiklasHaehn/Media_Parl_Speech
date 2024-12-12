5cfc59d4b8641a993e8f5a598857951618ee516a
Niklas Haehn <88054870+NiklasHaehn@users.noreply.github.com>
2024-12-12 10:40:09 -0500

    Update 01_Scraping_newspapers.R
    Change to actual R scripts

--- 01_Scraping_newspapers.R
+++ 01_Scraping_newspapers.R
@@ -1,70 +1,162 @@
+setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
 
-install.packages("rvest")
-install.packages("RSelenium")
+library(dplyr)
+library(purrr)
 library(rvest)
-library(RSelenium)
+library(xml2)
+library(furrr)
+library(progressr)
 
+# Sitemaps are numbered by year, but there is no
+# obvious convention, so we are guessing numbers:
+base_url <- "https://www.sueddeutsche.de/sitemapapp/sitemap/article/"
+all <- expand.grid(as.character(2009:2012), as.character(1:20)) |>  arrange(Var1)
+sitemaps <- paste0(base_url, all$Var1, "/", all$Var2)
 
-driver <- rsDriver(browser = "chrome")
-remDr <- driver[["client"]]
-
-
-remDr$navigate("https://www.example.com")
-
-
-Sys.sleep(5)
-
-
-start_date <- as.Date(readline("Gib das Startdatum im Format YYYY-MM-DD ein: "))
-end_date <- as.Date(readline("Gib das Enddatum im Format YYYY-MM-DD ein: "))
-
-
-date_sequence <- seq(start_date, end_date, by = "days")
-
-
-for (selected_date in date_sequence) {
-  date_input <- remDr$findElement(using = "css", value = "your-date-selector")
-  date_input$clickElement()
-  date_input$clearElement()
-  date_input$sendKeysToElement(list(as.character(selected_date)))
+get_urls <- function(sitemap) {
+  out <- 
+    sitemap |> 
+    html_elements("url") |> 
+    html_element("loc") |> 
+    html_text2()
   
-
-  Sys.sleep(5)
-  
- 
-  link_elements <- remDr$findElements(using = "css", value = "your-link-selector")
-  
-
-  links <- sapply(link_elements, function(el) el$getElementAttribute("href")[[1]])
-  
-
-  for (link in links) {
-
-    remDr$navigate(link)
-    
-
-    Sys.sleep(5)
-    
-
-    webpage <- remDr$getPageSource()[[1]]
-    
-
-    page_content <- read_html(webpage)
-    
-
-    titles <- page_content %>% html_nodes("h1") %>% html_text()
-    
-
-    print(titles)
-    
-
-    remDr$goBack()
-    
-
-    Sys.sleep(3)
-  }
+  if (rlang::is_empty(out)) return(NA_character_)
+  out
 }
 
+try_sitemaps <- function(url, p) {
+  p()
+  
+  tryCatch(
+    expr = {
+      url |> 
+        rvest::read_html() |> 
+        get_urls()
+    },
+    error = function(e) return()
+  )
+}
 
-remDr$close()
-driver$server$stop()
+plan(multisession, workers = parallel::detectCores())
+
+handlers(
+  handler_progress(
+    format = "[:bar] Remaining: :eta"
+  )
+)
+
+with_progress({
+  p <- progressor(steps = length(sitemaps))
+  article_urls <- sitemaps |> future_map(\(.x) try_sitemaps(.x, p = p))
+})
+
+article_urls <- do.call(c, article_urls)
+
+# There is a problem with SZ
+# where return values get multiplied
+# for no obvious reasons. Guarding
+# against this, as well as empty
+# returns:
+guard <- function(x) {
+  if (rlang::is_empty(x))
+    return(NA_character_)
+  else if (length(x) > 1)
+    return(x[1])
+  
+  x
+}
+
+# Functions for information retrieval 
+get_title <- function(src) {
+  src |> 
+    html_element("span") |>
+    html_element(xpath = '//*[@data-manual="title"]') |> 
+    html_text2() |> 
+    guard()
+}
+
+get_meta <- function(src, query) {
+  src |> 
+    html_elements("meta") |> 
+    html_element(xpath = paste0('//*[@name="', query, '"]')) |> 
+    html_attr("content") |> 
+    guard()
+}
+
+get_date <- function(src) {
+  src |> 
+    html_elements("time") |> 
+    xml_attr("datetime") |> 
+    guard()
+}
+
+check_paywall <- function(src)
+  grepl('"page.paywall_shown":true', html_text(src))
+
+get_body <- function(src) {
+  src |>
+    html_elements("p") |> 
+    html_element(xpath = '//*[@data-manual="paragraph"]') |> 
+    (\(.x) .x[1])() |> 
+    html_text2()
+}
+
+# Full scrape
+
+scrape_article <- function(url) {
+  article <- read_html(httr::GET(url))
+  
+  out <- data.frame(
+    url         = url,
+    title       = get_title(article),
+    date        = get_date(article), 
+    description = get_meta(article, "description"),
+    keywords    = get_meta(article, "keywords"),
+    author      = get_meta(article, "author"),
+    paywall     = check_paywall(article),
+    body        = NA_character_,
+    error       = NA_character_
+  ) 
+  
+  # retrieve article body if no paywall:
+  out$body <- ifelse(out$paywall, out$body, get_body(article))
+  
+  out
+}
+
+# This is what to do if an error occurs:
+error_handler <- function(url, error_obj) {
+  # log url & error message:
+  msg <- as.character(error_obj$message)
+  df <- data.frame(url = url, error = msg)
+
+  # fill everything else with NA:
+  to_fill <- c(
+    "title", "author", "date", "description",
+    "keywords", "paywall", "body"
+  )
+  df[, to_fill] <- NA_character_
+
+  df
+}
+
+# ...putting it together:
+scrape_safely <- function(url, p) {
+  p() 
+  
+  tryCatch(
+    expr = scrape_article(url), 
+    error = \(e) error_handler(url = url, error_obj = e)
+  )
+}
+
+with_progress({
+  p <- progressor(steps = length(article_urls))
+  sueddeutsche <- article_urls |> future_map(\(.x) scrape_safely(.x, p = p))
+})
+
+sueddeutsche_1 <- map(sueddeutsche, ~ mutate(.x, paywall = as.logical(paywall)))
+
+sueddeutsche_1 |> 
+  bind_rows() |>
+  write.csv("sueddeutsche.csv")
